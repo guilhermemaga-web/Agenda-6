22b39f72fd8099b9de440220019ed61d30aec535
Guilherme <guilhermemaga97@gmail.com>
2026-09-15 18:47:02 -0300

    desconto

--- desconto.py
+++ desconto.py
@@ -3,9 +3,7 @@ total_compra = float(input("digite o valor total da compra:"))
 
 #condições para desconto
 if total_compra < 200:
-    print("você recebeu 5% de desconto:")
     desconto = total_compra *0.05
-    print("Valor final:", desconto) 
 elif total_compra >=200 and total_compra <300:
     desconto = total_compra *0.10
 else:
